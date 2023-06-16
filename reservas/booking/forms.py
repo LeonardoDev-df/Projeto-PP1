ef4b7c32cfa93c86c51bc620e1bbecf2120f4056
Leonardo Lopes <leonardolopesborges@hotmail.com>
2023-06-16 13:51:47 -0300

    Upgrade tela criar reservas
    responsividade e css

--- reservas/booking/forms.py
+++ reservas/booking/forms.py
@@ -4,4 +4,5 @@ from .models import Reserva
 class ReservaForm(forms.ModelForm):
     class Meta:
         model = Reserva
-        fields = ('nome', 'email', 'numero_pessoas', 'data', 'horario', 'mesas_reservadas')
+        fields = ('data', 'nome', 'email', 'numero_pessoas', 'horario')
+        
