d702fb69caafe57192f69b2f64d6990bb487dbaf
Leonardo Lopes <leonardolopesborges@hotmail.com>
2023-06-19 14:43:53 -0300

    deploy

--- reservas/reservas/settings.py
+++ reservas/reservas/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-h-2gbz+-+)#q7)oz$61i#os$9x(^cui4q*bjppwg5-)h%&#(s-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
